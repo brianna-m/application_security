575bcb05b7980a96594231067c57b6c2893e561f
Brianna (she/her) <114614494+brianna-m@users.noreply.github.com>
2024-01-14 22:00:05 -0800

    Update/upload app.py

--- app.py
+++ app.py
@@ -3,17 +3,23 @@
 # Winter 2024 TESC
 # https://canvas.evergreen.edu/courses/6279/assignments/111546
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='')
+app = Flask(__name__, template_folder='./templates',  static_folder='./static')
 
 @app.route('/')
-def hello_world(): # put application's code here
-    return "Hello World"
+def home(): # Task 4, Item 1
+    return render_template('index.html', name='not_sure_what_this_name_is_for')
 
 @app.route('/index')
 def index():
-    return render_template(index.html, name='PyCharm')
+    return render_template('index.html', name='PyCharm')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        user_input = request.form['w1_input']
+        return render_template('submission_result.html', user_input=user_input)
 
 if __name__ == '__main__':
     app.run()
